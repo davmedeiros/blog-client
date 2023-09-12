c4da97690b33dddb7254be04c1f6c79789ca58d5
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-12 20:59:15 +0100

    Clean up generated css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
