3c404dad6ec143a0612d29b790b4a8a0ec8712cd
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-13 08:52:55 +0100

    Add test api call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [message, setMessage] = useState();
+
+  const getUser = async () => {
+    const response = await fetch('http://localhost:3000', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setMessage(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>Hello</h1>
       <Link to="/login">Login</Link>
     </>
   );
